d481090e0cc655ce6fbfe63965d1239e3114a275
Pedro Lourenço <padl@gmv.com>
2025-10-16 19:01:36 +0100

    update dual gap function example to showcase the embeddable file generation process

--- examples/optimization/pipg/gapfun.m
+++ examples/optimization/pipg/gapfun.m
@@ -1,6 +1,6 @@
-function y=gapfun(w,z)
+function f=gapfun(w,z)
 
 zaux = setfun(z);
-y=w'*conefun(z) +z'*zaux;
+f=w'*conefun(z) +z'*zaux;
 
 end
--- examples/optimization/pipg/main.m
+++ examples/optimization/pipg/main.m
@@ -7,3 +7,9 @@ w = adigatorCreateAuxInput([2 1]);
 opts.embed_mode = 'l';
 
 adigatorGenDerFile_embedded('hessian','gapfun',{w,z},opts);
+
+w = randn(2,1); z = randn(2,1);
+
+gapfun(w,z)
+[H,G,f] = gapfun_Hes(w,z)
+[G,f] = gapfun_Grd(w,z)
